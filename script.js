313e50e7712bb864bbde65bd5054927cc5484d70
Jesus Daniel Gonzalez Bautista <121988244+narvenio@users.noreply.github.com>
2024-09-09 07:06:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -28,27 +28,31 @@ let aumento;
 
 function scroll(){
    
-      window.scrollTo(0, 1000);
-      function delay(ms){
-        return new Promise(resolve => setTimeout(resolve, ms));
-      }
      
-      async function mostrar_scroll() {
-        console.log("inicio");
-     
-        // espera 2 segundos
-        await delay(500)
-     
-        console.log("despues de 2 segundos")
      
         // continuar con el resto del codigo
         
         window.scrollTo(0, 1000);
       }
       
-}
 
 
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+ 
+  async function mostrar_scroll() {
+    console.log("inicio");
+ 
+    // espera 2 segundos
+    await delay(500)
+ 
+    console.log("despues de 2 segundos")
+ 
+    // continuar con el resto del codigo
+    
+    window.scrollTo(0, 1000);
+  }
 
 function borrar(){
     document.getElementById("textbox1").value="";
